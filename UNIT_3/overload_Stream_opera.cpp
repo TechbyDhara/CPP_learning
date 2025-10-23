61bf4f5840427b743ce20bd1c83b3faf81fc3f07
Dhara <dharakateshiya77@gmail.com>
2025-10-23 09:50:48 +0530

    stream_<<_operator

---
+++ UNIT_3/overload_Stream_opera.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    int age;
+public:
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    friend ostream& operator << (ostream &out,Student &s);
+};
+ostream& operator << (ostream &out, const Student &s) {
+    out << "Name: " << s.name << ", Age: " << s.age;
+    return out;  
+}
+int main() {
+    Student s1("Dhara", 20);
+    cout << s1;   
+    return 0;
+}
+
